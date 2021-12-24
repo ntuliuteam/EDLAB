453451e3d6673f872e8f8cf44038fda2768a802b
Huai Shuo <54792401+SHUO-HUAI@users.noreply.github.com>
2021-12-24 09:50:42 +0800

    Change License

--- scripts/tpu/classification/convertToTPUModelFromPb.py
+++ scripts/tpu/classification/convertToTPUModelFromPb.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) HP-NTU Digital Manufacturing Corporate Lab, Nanyang Technological University, Singapore.
 #
-# This source code is licensed under the MIT license found in the
+# This source code is licensed under the Apache-2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 
 import numpy as np
